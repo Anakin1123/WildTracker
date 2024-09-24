39e67c491e17b15b3bcf8fce4ca347cf5533782d
Student <Student@example.com>
2024-09-24 13:10:11 +0300

    изминения в html файлах

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
-zxc = FastAPI()
 
-zxc.mount("/static", StaticFiles(directory="static"), name="static")
-# Инициализируем Jinja2Templates с директорией templates
+# Инициализация приложения FastAPI
+app = FastAPI()
+
+# Инициализация маршрутизатора для работы со студентами
+router = APIRouter(prefix='/students', tags=['Работа со студентами'])
+
+# Подключение статических файлов
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Инициализация Jinja2Templates с директорией templates
 templates = Jinja2Templates(directory="templates")
 
-@zxc.get("/")
+# Главная страница
+@app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@zxc.get("/about")
+# Страница "О нас"
+@app.get("/about")
 async def about(request: Request):
     return templates.TemplateResponse("about.html", {"request": request})
 
-@zxc.get("/contact")
+# Страница "Контакты"
+@app.get("/contact")
 async def contact(request: Request):
     return templates.TemplateResponse("contact.html", {"request": request})
 
-@zxc.get("/post")
+# Страница блога
+@app.get("/post")
 async def post(request: Request):
     return templates.TemplateResponse("post.html", {"request": request})
+
+# Подключение маршрутизатора (если потребуется в будущем)
+app.include_router(router)
