e9abebc5694d4ee74b9f8e506e38994fa140d47c
Student <Student@example.com>
2024-10-07 15:15:48 +0300

    main fix

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from numbers import Real
+from fastapi import Depends, FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
+from fastapi_users import FastAPIUsers
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserCreate
 from routers.router import router  # Импортируйте ваш маршрутизатор
 
 # Инициализация приложения FastAPI
@@ -35,3 +40,31 @@ async def post(request: Request):
 
 # Подключение маршрутизатора (если потребуется в будущем)
 app.include_router(router, prefix="/api")
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(Real, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, anonym"
